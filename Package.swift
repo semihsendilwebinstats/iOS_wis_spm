ded98b8efb1e3132afde9217d26a95f8077bb1aa
wisselin <selin@webinstats.com>
2022-11-11 16:57:40 +0300

    syntax

--- Package.swift
+++ Package.swift
@@ -2,18 +2,17 @@
 // swift-tools-version:5.3
 import PackageDescription
 let package = Package(
-	name: "iOS_wis"
+	name: "iOS_wis",
 	platforms:[
 		.iOS(.v11)
 	],
 	products: [
 		.library(
-			name:"iOS_wis"
+			name:"iOS_wis",
 			targets: ["iOS_wis"])
 	],
 	targets: [
 		.binaryTarget(
-			name:"iOS_wis"
+			name:"iOS_wis",
 			path:"iOS_wis.xcframework")
 	])
-EOF
